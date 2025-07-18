b07056081727e3a6ce16fa7dacc75f5d912dfbbc
Zoxics <jackzoxics@gmail.com>
2025-07-17 22:37:41 -0400

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import ContentGrid from "./components/ContentGrid";
-import Projects from "./components/Projects";
 import TopNav from "./components/TopNav";
 import { Toaster } from "sonner";
 

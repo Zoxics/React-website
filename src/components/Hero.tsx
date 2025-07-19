74fab0f4832b1646784cb60e4e5dbbf273e9f092
Jack Nargi <95267091+Zoxics@users.noreply.github.com>
2025-07-18 22:14:06 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,7 @@ function Hero() {
           </button>
 
           <a
-            href="/Jonathan Nargi Resume 2025.pdf"
+            href="`${import.meta.env.BASE_URL}Jonathan-Nargi-Resume-2025.pdf`"
             download
             className="flex flex-row items-center gap-2 bg-primary text-light px-5 py-2 sm:px-6 sm:py-3 text-lg sm:text-2xl tracking-wide shadow-md hover:bg-orange-700 transition-all duration-200"
           >
